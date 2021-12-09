075b7a8742fb6d11894aad564c1df338b42556df
ChrisCh <42211808+ChrisCh7@users.noreply.github.com>
2021-12-09 09:44:28 +0200

    Refactor code

--- 2021/day1/day1.hs
+++ 2021/day1/day1.hs
@@ -6,14 +6,14 @@ main :: IO ()
 main = do
   contents <- readFile "in.txt"
   let depths = map (\line -> read line :: Int) $ lines contents
-  putStrLn $ "Part 1: " ++ show (day1 depths)
-  putStrLn $ "Part 2: " ++ show (day2 depths)
+  putStrLn $ "Part 1: " ++ show (part1 depths)
+  putStrLn $ "Part 2: " ++ show (part2 depths)
 
-day1 :: [Int] -> Int
-day1 depths = foldl (\acc i -> if (depths !! i) > (depths !! (i - 1)) then acc + 1 else acc) 0 [1 .. length depths - 1]
+part1 :: [Int] -> Int
+part1 depths = foldl (\acc i -> if (depths !! i) > (depths !! (i - 1)) then acc + 1 else acc) 0 [1 .. length depths - 1]
 
-day2 :: [Int] -> Int
-day2 depths = foldl (\acc i -> if sum3 depths i > sum3 depths (i - 1) then acc + 1 else acc) 0 [3 .. length depths - 1]
+part2 :: [Int] -> Int
+part2 depths = foldl (\acc i -> if sum3 depths i > sum3 depths (i - 1) then acc + 1 else acc) 0 [3 .. length depths - 1]
 
 sum3 :: [Int] -> Int -> Int
 sum3 depths i = depths !! i + depths !! (i - 1) + depths !! (i - 2)
