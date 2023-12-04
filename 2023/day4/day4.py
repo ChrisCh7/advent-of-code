6a4c6337808aac9cef12c5d9cd113eb428005b7d
ChrisCh <42211808+chrisch7@users.noreply.github.com>
2023-12-04 19:23:46 +0200

    Optimize 2023 day 4 solution

--- 2023/day4/day4.py
+++ 2023/day4/day4.py
@@ -1,8 +1,7 @@
-import copy
 import re
 
 
-def part1(cards: list[list[list[int]]]):
+def part1(cards: list[tuple[tuple[int, ...], ...]]):
     points_total = 0
 
     for winning_nrs, owned_nrs in cards:
@@ -20,22 +19,21 @@ def part1(cards: list[list[list[int]]]):
     print('Part 1:', points_total)
 
 
-def part2(cards: list[list[list[int]]]):
-    cards_og = copy.deepcopy(cards)
+def part2(cards: list[tuple[tuple[int, ...], ...]]):
+    cards_og = cards.copy()
+
+    card_idxs = {card: i for i, card in enumerate(cards)}
 
     nr_cards = 0
 
     for card in cards:
         nr_cards += 1
-        winning_nrs, owned_nrs = card
-        matching_nrs = 0
+        winning_nrs, owned_nrs = map(set, card)
 
-        for nr in owned_nrs:
-            if nr in winning_nrs:
-                matching_nrs += 1
+        matching_nrs = len(winning_nrs & owned_nrs)
 
         for i in range(matching_nrs):
-            cards.append(cards_og[cards_og.index(card) + i + 1])
+            cards.append(cards_og[card_idxs[card] + i + 1])
 
     print('Part 2:', nr_cards)
 
@@ -45,7 +43,7 @@ if __name__ == '__main__':
         lines = file.read().splitlines()
 
     cards = [line.split(':')[1].split('|') for line in lines]
-    cards = [[list(map(int, re.findall(r'\d+', nrs))) for nrs in line] for line in cards]
+    cards = [tuple(tuple(map(int, re.findall(r'\d+', nrs))) for nrs in line) for line in cards]
 
     part1(cards)
     part2(cards)
